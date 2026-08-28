5e120daabca546180737a2251700e6b12ab8e917
mansiaggarwa12 <mansiaggarwal2081@gmail.com>
2026-08-29 00:02:12 +0530

    Time: 131 ms (67.17%), Space: 43.1 MB (28.18%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for(int r=0;r<board.length;r++){
+            for(int c=0;c<board[0].length;c++){
+                if(dfs(board,word,r,c,0))return true;
+            }
+        }
+        return false;
+    }
+    static boolean dfs(char [][]board, String word, int r, int c, int index){
+        if(index==word.length())return true;
+        if(r<0||r>=board.length||c<0||c>=board[0].length||board[r][c]!=word.charAt(index))return false;
+        char og = board[r][c];
+        board[r][c]='#';
+        boolean found = dfs(board,word,r+1,c,index+1)
+        || dfs(board,word,r-1,c,index+1)
+        || dfs(board,word,r,c+1,index+1)
+        || dfs(board,word,r,c-1,index+1);
+        board[r][c]=og;
+        return found;
+
+
+    }
+}
