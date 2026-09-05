bfef3c0f934537cce1a5f7c3cad9e7ea53a11a3e
mansiaggarwa12 <mansiaggarwal2081@gmail.com>
2026-09-05 19:36:07 +0530

    Time: 14 ms (96.23%), Space: 98.1 MB (27.09%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,20 +1,19 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> ans =  new ArrayList<>();
-        backtrack(n,k,1,new ArrayList<>(),ans);
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(n,k,1,ans,new ArrayList<>());
         return ans;
     }
-
-    static void backtrack(int n, int k, int start, List<Integer> path, List<List<Integer>> ans){
-        if(path.size()==k){
-            ans.add(new ArrayList<>(path));
-            return;
+    void backtrack(int n, int k, int i, List<List<Integer>> ans, List<Integer> curr){
+        if(curr.size()==k){
+            ans.add(new ArrayList<>(curr));
+            return ;
         }
-        int needed = k-path.size();
-        for(int value = start ;value<=n-needed+1;value++){
-            path.add(value);
-            backtrack(n,k,value+1,path,ans);
-            path.remove(path.size()-1);
+        int needed = k-curr.size();
+        for(int val = i;val<=n-needed+1;val++){
+            curr.add(val);
+            backtrack(n,k,val+1,ans,curr);
+            curr.remove(curr.size()-1);
         }
     }
 }
