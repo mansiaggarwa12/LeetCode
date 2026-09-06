4fb5ab17e67aadecdae765d3a5e3bc56205beb1b
mansiaggarwa12 <mansiaggarwal2081@gmail.com>
2026-09-06 13:02:34 +0530

    Time: 3 ms (93%), Space: 46.7 MB (43.1%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,43 +1,23 @@
 class Solution {
-    boolean[] used;
-    List<Integer> path;
-    List<List<Integer>> ans;
-
     public List<List<Integer>> permuteUnique(int[] nums) {
-
+        List<List<Integer>> ans = new ArrayList<>();
+        boolean []used = new boolean[nums.length];
         Arrays.sort(nums);
-
-        used = new boolean[nums.length];
-        path = new ArrayList<>();
-        ans = new ArrayList<>();
-
-        backtrack(nums);
-
+        backtrack(nums,new ArrayList<>(),used,ans);
         return ans;
     }
-
-    public void backtrack(int[] nums) {
-
-        if (path.size() == nums.length) {
+    void backtrack(int []nums ,List<Integer> path, boolean[]used,List<List<Integer>>ans ){
+        if(path.size()==nums.length){
             ans.add(new ArrayList<>(path));
-            return;
+            return ;
         }
-
-        for (int i = 0; i < nums.length; i++) {
-
-            if (used[i])
-                continue;
-
-            // Skip duplicate choices at the same level
-            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
-                continue;
-
-            used[i] = true;
+        for(int i=0;i<nums.length;i++){
+            if(i>0 && nums[i]==nums[i-1] && !used[i-1])continue;
+            if(used[i])continue;
+            used[i]=true;
             path.add(nums[i]);
-
-            backtrack(nums);
-
-            path.remove(path.size() - 1);
+            backtrack(nums,path,used,ans);
+            path.removeLast();
             used[i] = false;
         }
     }
