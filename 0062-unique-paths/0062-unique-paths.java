e3f26d5c6507d953b2a255b64558d1892d26f402
mansiaggarwa12 <mansiaggarwal2081@gmail.com>
2026-09-19 13:13:46 +0530

    Time: 0 ms (100%), Space: 41.8 MB (91.39%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,17 +1,12 @@
 class Solution {
-    public int uniquePaths(int rows, int cols) {
-        int [][] dp = new int [rows][cols];
-        for(int r=0;r<rows;r++){
-            dp[r][0]=1;
-        }
-        for(int c=0;c<cols;c++){
-            dp[0][c]=1;
-        }
-        for(int r=1;r<rows;r++){
-            for(int c=1;c<cols;c++){
-                dp[r][c]=dp[r-1][c]+dp[r][c-1];
+    public int uniquePaths(int m, int n) {
+        int [][] dp = new int [m][n];
+        for(int[] x:dp)Arrays.fill(x,1);
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
-        return dp[rows-1][cols-1];
+        return dp[m-1][n-1];
     }
 }
